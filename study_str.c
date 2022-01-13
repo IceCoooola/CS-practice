745bd14517e21ac745a38b0ed0a5230149c0e01b
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-01-13 00:13:32 -0800

    Update study_str.c

--- study_str.c
+++ study_str.c
@@ -5,6 +5,19 @@
 #include<string.h>
 #include<assert.h>
 
+void* my_memcpy(void*, const void*, size_t);
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//my_memcpy(arr + 2, arr, 20);
+	memcpy(arr + 2, arr, 20);
+	int i = 0;
+	for (i; i < 10; i++)
+		printf("%d ",arr[i]);
+	return 0;
+}
+
 void* my_memcpy(void* dest, const void* src, size_t sz)
 {
 	assert(dest&&src&&sz);
