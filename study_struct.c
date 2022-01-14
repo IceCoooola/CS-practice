547ece9b2f7a14f1cee6487156f603e65290cb1e
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-01-14 15:33:25 -0800

    Create study_struct.c

---
+++ study_struct.c
@@ -0,0 +1,28 @@
+struct S
+{
+	int a;
+	struct S* pS;
+};//结构体的自引用
+
+struct//不写结构体名字的是匿名结构体变量
+{
+	char name[20];
+	int age;
+	char describe[50];
+}sa;//匿名结构体变量只能在这儿创建变量名
+
+struct Stu
+{
+	char name[20];
+	int age;
+	char tel[11];
+	char gender[2];
+}a,b,c;//在这里创建的是全局变量
+
+struct Stu q[5];//在这里创建的也是全局变量
+
+int main()
+{
+	struct Stu s[5];//在这里创建的是局部变量
+	return 0;
+}
