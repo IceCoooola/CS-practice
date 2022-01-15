edd98e07958fe4532397b2a205d59a6e766a3922
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-01-15 01:53:47 -0800

    Update study_struct.c

--- study_struct.c
+++ study_struct.c
@@ -1,4 +1,39 @@
 
+typedef struct stu {
+	char a;
+	int b;
+	char c;
+}S;
+
+typedef struct stu2 {
+	char a;
+	char b;
+	int c;
+}S2;
+
+typedef struct stu3 {
+	int c;
+	char a;
+	char b;
+}S3;
+
+typedef struct stu4 {
+	int a;
+	char b;
+	double c;
+	char d;
+}S4;
+
+int main()
+{
+	printf("%d\n", sizeof(S));//结构体对其规则
+	printf("%d\n", sizeof(S2));//第一个成员放在结构体变量偏移量为0的地址处
+	printf("%d\n", sizeof(S3));//其他成员变量要对齐到（对齐数）的整倍地址处
+	printf("%d\n", sizeof(double));//对齐数：编译器默认的一个对齐数与该成员大小的较小值 ，VS默认8
+	printf("%d\n", sizeof(S4));//结构体总大小为最大对齐数的整倍数。
+	return 0;
+}
+
 struct T
 {
 	double q;
