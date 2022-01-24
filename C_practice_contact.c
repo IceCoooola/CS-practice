320885d5ffdf849c87b75830e93debb57f196db2
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-01-24 00:56:31 -0800

    Update C_practice_contact.c

--- C_practice_contact.c
+++ C_practice_contact.c
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-
+#include "contact.h"
 // 1. test.c 测试
 // 2. contact.c 实现函数的功能
 // 3. contact.h 声明函数
@@ -12,4 +12,23 @@
 // 5. 修改好友信息
 // 6. 打印好友信息
 // 7. 排序
-//
+void menu() {
+	printf("************************************\n");
+	printf("********Your contact list***********\n");
+	printf("*** 1.  Add  ******* 2. Delete *****\n");
+	printf("*** 3. Find  ******* 4. Modify *****\n");
+	printf("*** 5. Print ******* 0. Quit   *****\n");
+	printf("************************************\n");
+}
+int main()
+{
+
+	int input = 0;
+	do {
+		menu();
+		printf("Please choose>:");
+		scanf("%d", &input);
+	}
+		while(input);
+	return 0;
+}
