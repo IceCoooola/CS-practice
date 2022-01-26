df9f0591db2ad8ee2b99416f9d5c650b7d874368
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-01-26 15:22:01 -0800

    Update Cpp_practice.cpp

--- Cpp_practice.cpp
+++ Cpp_practice.cpp
@@ -5,6 +5,25 @@ using namespace std;
 
 int main()
 {
+	int sum = 0;
+	cout << "Please enter an array of intergers and any spaces.:>";
+	int i;
+	while (cin >> i)
+	{
+		sum += i;
+		while (cin.peek() == ' ')//cin.peek 是瞅一眼缓冲区的是不是' '
+		{
+			cin.get();//cin.get就是把第一个字符从缓冲区拿出来，相当于getchar
+		}
+		if (cin.peek() == '\n')//瞅一眼如果是\n就跳出循环
+			break;
+	}
+	cout << "The sum is:" << sum << endl;
+	return 0;
+}
+
+int main()
+{
 	int a = 0;
 	cout << "hello world\n";
 	cin >> a;
