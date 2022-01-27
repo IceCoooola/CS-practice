70ecdc5d230ba96642085b8c3b32e68d1ad322de
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-01-26 16:01:57 -0800

    Update Cpp_practice.cpp

--- Cpp_practice.cpp
+++ Cpp_practice.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main()
 {
+	char buf[20];
+	cin.ignore(7);//忽略前7个字符
+	cin.getline(buf, 10);//提取10个字符（实际是9个，因为最后一个默认是\0
+	cout << buf<<endl;//这里输出提取到的9个字符
+	return 0;
+}
+
+int main()
+{
 	int sum = 0;
 	cout << "Please enter an array of intergers and any spaces.:>";
 	int i;
