273b7c4a7cebd6341530750da7b3ce4fbb095e92
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-01-28 00:01:48 -0800

    Update study_C_dynamic_memory.c

--- study_C_dynamic_memory.c
+++ study_C_dynamic_memory.c
@@ -1,3 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<errno.h>
+
+int main()
+{
+	int* p = malloc(10 * sizeof(int));
+	if (p == NULL)
+	{
+		printf("%s\n", strerror(errno));
+	}
+	else 
+	{
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			*(p + i) = i;
+		}	
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d ", *(p+i));
+		}
+		printf("\n");
+	}
+	//如果这时候o开辟的空间不够用，可以用realloc加大
+	int* p2 = realloc(p, 20 * sizeof(int));//realloc调整这块空间大小到后面的bytes
+	//realloc返回一个新的地址，有可能是之前的，有可能是新创建的
+	//如果realloc后面有足够的空间追加，那就追加上然后返回p
+	//realloc后面没有足够的空间追加，那就重新找一块区域给之前的搬过去，（之前的释放掉）然后返回新的地址
+	//realloc开辟后要用新的指针接收，因为realloc可能内存不够开辟失败，开辟失败会返回NULL，如果返回原来的指针，之前那块就也丢了找不到了
+	int i;
+	if (p2 == NULL)
+	{
+		printf("%s\n", strerror(errno));
+	}
+	else 
+	{
+		p = NULL;
+		for (i = 0; i < 20; i++)
+		{
+			printf("%d ", *(p2 + i));
+		}
+		printf("\n");
+		for (i = 10; i < 20; i++)
+		{
+			*(p2 + i) = i;
+		}
+		for (i = 0; i < 20; i++)
+		{
+			printf("%d ", *(p2 + i));
+		}
+		printf("\n");
+	}
+	free(p2);
+	p2 = NULL;
+	return 0;
+}
+
 
 int main()
 {
