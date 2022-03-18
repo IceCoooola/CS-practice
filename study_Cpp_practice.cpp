c51dc0746f78c7dd8570f1475ff7071ba58a4507
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-03-17 23:04:16 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -28,6 +28,14 @@ int main()
 	point1.MovePoint(2, 2);
 	point1.Print();
 	return 0;
+	//	//当point1调用MovePoint函数的时候，即将point1对象的地址传给了this指针。
+//	//MovePoint函数的原型事实上应该是void MovePoint(Point* this, int a, int b)
+//	//第一个参数是指向该类对象的一个指针，我们在定义成员函数的时候没看见是因为这个参数在类(class)中是隐含的
+//	//这样point1的地址传给了this，所以在MovePoint函数中便可以显示的写成：
+//	//void MovePoint(int a, int b) 
+//	//{ this->x = a; this->y = b;}
+//	//既可以知道，point1调用该函数后，也就是point1的数据成员被调用并更新了值。
+
 }
 
 #define _CRT_SECURE_NO_WARNINGS
