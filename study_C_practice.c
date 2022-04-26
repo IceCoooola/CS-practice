bb9241491a94f83be0d1358152c23191fa9a44b8
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-04-25 22:40:24 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -8,6 +8,22 @@
 
 //Middle of the Linked List.
 
+
+
+struct ListNode* middleNode(struct ListNode* head){
+   
+    struct ListNode* fast = head;
+    struct ListNode* slow = head;
+    while(fast&&fast->next)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+//Middle of the Linked List.
+
 struct ListNode* middleNode(struct ListNode* head){
    
     int sz = 0;
