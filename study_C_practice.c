d37140adbe072c394d745a252bf51b34df6b6418
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-04-28 23:33:06 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -1,10 +1,52 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
+
+//merge two sorted lists
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
+    if(list1 == NULL)
+    {
+        return list2;
+    }
+    if(list2 == NULL)
+    {
+        return list1;
+    }
+    struct ListNode* head = NULL;
+    struct ListNode* tail = NULL;
+    if(list1->val > list2->val)
+    {
+        head = list2;
+        tail = head;
+        list2 = list2->next;
+    }
+    else
+    {
+        head = list1;
+        tail = head;
+        list1 = list1->next;
+    }
+    while(list1 && list2)
+    {
+        if(list1->val > list2->val)
+        {
+            tail->next = list2;
+            list2 = list2->next;
+        }
+        else
+        {
+            tail->next = list1;
+            list1 = list1->next;
+        }
+            tail = tail->next;
+    }
+    if(list1)
+    {
+        tail->next = list1;
+    }
+    else
+    {
+        tail->next = list2;
+    }
+    return head;
+}
 
 //Middle of the Linked List.
 
