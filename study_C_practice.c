352e45da0c54a4b97adca2ad4486ed535fbc150a
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-04-30 00:42:09 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -1,4 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int main()
+{
+	int arr[10][10] = { 0 };
+	arr[0][0] = 1;
+	for (int i = 1; i < 10; i++)
+	{
+		arr[i][0] = 1;
+		int j;
+		for (j = 1; j < i; j++)
+		{
+			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+		}
+		arr[i][j] = 1;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < i+1; j++)
+		{
+			printf("%d ",arr[i][j]);
+
+		}
+		printf("\n");
+	}
+	return 0;
+}
 //merge two sorted lists
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
     if(list1 == NULL)
