b61ff8afee984c6038fe9aec7e4e891385d8bda2
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-05-12 21:38:51 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -5,6 +5,18 @@
  *     struct ListNode *next;
  * };
  */
+void deleteNode(struct ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 
 
 struct ListNode* removeElements(struct ListNode* head, int val){
