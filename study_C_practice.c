93f967d5435ed12c570367157ea0fe85bf39aabb
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-05-24 17:47:20 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -1,3 +1,26 @@
+
+
+int removeElement(int* nums, int numsSize, int val){
+
+    int fast = 0;
+    int slow = 0;
+    int count = 0;
+    while(fast < numsSize)
+    {
+        if(nums[fast] == val)
+        {
+            fast++;
+        }
+        else
+        {
+            nums[slow] = nums[fast];
+            count++;
+            fast++;
+            slow++;
+        }
+    }
+    return count;
+}
 TreeKLevelSize(BTNode* root, int k)
 {
 	if(k ==1)
