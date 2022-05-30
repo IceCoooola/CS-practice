898bab659b9bc9a38fb1a9fcd40a185b53169d19
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-05-29 20:36:56 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -1,4 +1,22 @@
 
+struct TreeNode
+{
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+	if(p == NULL && q == NULL)
+		return true;
+	if (p == NULL || q == NULL)
+		return false;
+	if (p->val != q->val)
+		return false;
+	return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
 
 void swap(int* num1, int* num2)
 {
