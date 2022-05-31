749930eae94b83e70050e2b3b56ee67195174f66
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-05-31 02:08:34 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -8,6 +8,15 @@
  */
 
 
+int maxDepth(struct TreeNode* root){
+
+    if(root == NULL)
+        return 0;
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+    return fmax(leftDepth, rightDepth) +1;
+}
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
