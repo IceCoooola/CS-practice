ec3bfa7b938c4422b18228698ec0b350185b4990
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-05-31 20:31:58 -0700

    Update study_C_practice.c

--- study_C_practice.c
+++ study_C_practice.c
@@ -6,6 +6,33 @@
  *     struct TreeNode *right;
  * };
  */
+
+void _invertTree(struct TreeNode* root)
+ {
+    if(root == NULL)
+        return;
+    if(root->left == NULL && root->right == NULL)
+        return;
+    struct TreeNode* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    _invertTree(root->left);
+    _invertTree(root->right);
+ }
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    _invertTree(root);
+    return root;
+}
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
 bool _isSymmetric(struct TreeNode* left, struct TreeNode* right)
 {
     if(left == NULL && right == NULL)
