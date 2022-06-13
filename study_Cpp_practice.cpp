ef0e43830fa626ec1a07799b95396b20360abb49
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-06-13 13:03:38 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -2,7 +2,54 @@
 #include<iostream>
 #include<assert.h>
 #include<string>
+using namespace std;
+int main(int argc, char* argv[])
 
+{
+
+	string a = "hello world";
+
+	string b = a;
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	string c = b;
+
+	c = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	a = "";
+
+	if (a.c_str() == b.c_str())
+
+	{
+
+		cout << "true" << endl;
+
+	}
+
+	else cout << "false" << endl;
+
+	return 0;
+
+}
 int main()
 {
 	//三种最常用的构造函数
