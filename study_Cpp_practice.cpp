6083f36e45400609e1227dcb742ebd5d49ccd261
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-06-14 17:23:04 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,5 +1,34 @@
 class Solution {
 public:
+    void reverse(string& s, int left, int right)
+    {
+        while(left < right)
+        {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+    
+    string reverseStr(string s, int k) {
+        int prev = 0;
+        int cur = k-1;
+        while(cur < s.size())
+        {
+            reverse(s, prev, cur);
+            prev+=(2*k);
+            cur+=(2*k);
+        }
+        if(prev < s.size())
+        {
+            reverse(s, prev, s.size()-1);
+        }
+        return s;
+    }
+};
+
+class Solution {
+public:
     bool isSameLetter(char a, char b)
     {
         if( 'A' <= a && a <= 'Z')
