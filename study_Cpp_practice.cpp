0116c4b5d7b21e5f5706985466643405c56bc311
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-06-28 21:46:15 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -2,6 +2,39 @@ int main()
 
 {
 
+int array[] = { 1, 2, 3, 4, 0, 5, 6, 7, 8, 9 };
+
+int n = sizeof(array) / sizeof(int);
+
+list<int> mylist(array, array+n);
+
+auto it = mylist.begin();
+
+while (it != mylist.end())
+
+{
+
+if(* it != 0)
+
+cout<<* it<<" ";
+
+else
+
+it = mylist.erase(it);
+
+++it;
+
+}
+
+return 0;
+
+}
+
+
+int main()
+
+{
+
 int ar[] = { 0,1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 int n = sizeof(ar) / sizeof(int);
