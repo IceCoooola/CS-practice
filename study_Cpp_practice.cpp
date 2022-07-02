c4695cd9e101cc9972a364c7e35be129a1e3e885
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-07-01 22:39:25 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,4 +1,35 @@
-﻿template<typename T>
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        
+    }
+    
+    void pop() {
+        
+    }
+    
+    int top() {
+        
+    }
+    
+    int getMin() {
+        
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+template<typename T>
 
 void removeDuplicates(list<T> &aList)
 
