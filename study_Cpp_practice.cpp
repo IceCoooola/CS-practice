cafaef31546a4c617c0361ec04bcd2b247aa3ac8
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-07-01 23:07:51 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -5,22 +5,52 @@ public:
     }
     
     void push(int val) {
-        
+        st.push(val);
+        if(minVal.empty())
+        {
+            minVal.push_back(val);
+        }
+        else
+        {
+            if(st.top() <= minVal[minVal.size()-1])
+            {
+                minVal.push_back(val);
+            }
+        }
     }
     
     void pop() {
-        
+        if(minVal[minVal.size()-1] == st.top())
+        {
+            minVal.pop_back();
+            st.pop();
+        }
+        else
+        {
+            st.pop();
+        }
     }
     
     int top() {
-        
+        return st.top();
     }
     
     int getMin() {
-        
+        return minVal[minVal.size()-1];
     }
+private:    
+    stack<int> st;
+    vector<int> minVal;
 };
 
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
