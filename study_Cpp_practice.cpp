a1c35ba00bcd8f358e15027b31ded752a96e5430
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-07-08 09:35:07 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,4 +1,37 @@
-﻿void main()
+void main()
+
+{
+
+queue<char> Q;
+
+char x,y;
+
+x='n';y='g';
+
+Q.push(x);Q.push('i');Q.push(y);
+
+Q.pop();Q.push('r');Q.push('t');Q.push(x);
+
+Q.pop();Q.push('s');
+
+while(!Q.empty())
+
+{
+
+x = Q.front();
+
+Q.pop();
+
+cout<<x;
+
+};
+
+cout<<y;
+
+}
+
+
+void main()
 
 {
 
