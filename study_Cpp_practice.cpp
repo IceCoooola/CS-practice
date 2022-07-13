7134304507f82152397f76b162fbc860d7283dc9
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-07-12 17:49:15 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,3 +1,17 @@
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue q(nums.begin(), nums.end());
+        for(int i = 1; i<k;i++)
+        {
+            q.pop();
+        }
+        return q.top();
+        }
+};
+
+
 class Solution {
 
 public:
