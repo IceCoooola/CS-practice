3613b362a58180acb0e723052173f9bb3521fe9c
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-07-14 22:46:27 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,4 +1,73 @@
+class MyStack {
+public:
+    MyStack() 
+    {
+    }
+    
+    void push(int x) {
+        if(q1.empty())
+        {
+            q2.push(x);
+        }
+        else
+        {
+            q1.push(x);
+        }
+    }
+    
+    int pop() {
+        int tmp;
+        if(q1.empty())
+        {
+            while(q2.size() != 1)
+            {
+                q1.push(q2.front());
+                q2.pop();
+            }
+            tmp = q2.front();
+            q2.pop();
+        }
+        else
+        {
+            while(q1.size() != 1)
+            {
+                q2.push(q2.front());
+                q1.pop();
+            }
+                        tmp = q1.front();
+            q1.pop();
+            
+        }
+        return tmp;
+    }
+    
+    int top() {
+        if(q1.empty())
+        {
+            return q2.back();
+        }
+        else
+        {
+            return q1.back();
+        }
+    }
+    
+    bool empty() {
+        return q1.empty()&&q2.empty();
+    }
+    private:
+            queue<int> q1;
+        queue<int> q2;
+};
 
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
   template<class T, class Con = deque<T>>
 
   class queue
