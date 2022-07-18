64a2ba67f7d368b7512141cd898340e24586ef5d
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-07-17 21:37:21 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,3 +1,55 @@
+class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+            s2.push(s1.top());
+            s1.pop();                
+            }
+        }
+        int tmp = s2.top();
+        s2.pop();
+        return tmp;
+    }
+    
+    int peek() {
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+            s2.push(s1.top());
+            s1.pop();                
+            }
+        }
+        return s2.top();
+    }
+    
+    bool empty() {
+        return s1.empty()&&s2.empty();
+    }
+    private:
+    stack<int> s1;
+    stack<int> s2;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
 class MyStack {
 public:
     MyStack() 
