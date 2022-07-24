6aadf0d00f4cb1706f48b01f2a883d3a37907b6b
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-07-23 21:27:50 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,3 +1,53 @@
+
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(min.empty())
+        {
+            min.push(val);
+        }
+        else
+        {
+            if(min.top() >= st.top())
+            {
+                min.push(val);
+            }
+        }
+    }
+    
+    void pop() {
+        if(st.top() == min.top())
+        {
+            min.pop();
+        }
+        st.pop();
+        
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return min.top();
+    }
+    stack<int> st;
+    stack<int> min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
 class Solution {
 public:
     bool IsPopOrder(vector<int> pushV,vector<int> popV) {
