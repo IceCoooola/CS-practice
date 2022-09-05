aa95e84cbbb3fe8f6dfca92ade755df3f0044618
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-09-04 21:43:46 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -4,6 +4,46 @@ class A
 
 public:
 
+  A() { cout<<"A::A()"<<endl; }
+
+  ~A() { cout<<"A::~A()"<<endl; }
+
+  int a;
+
+};
+
+
+
+class B : public A
+
+{
+
+public:
+
+  B() { cout<<"B::B()"<<endl; }
+
+  ~B() {cout<<"B::~B()"<<endl; }
+
+  int b;
+
+};
+
+
+
+void f()
+
+{
+
+  B b;
+
+}
+
+class A
+
+{
+
+public:
+
   void f(){ cout<<"A::f()"<<endl; }
 
   int a;   
