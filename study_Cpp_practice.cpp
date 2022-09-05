29f21be804d1bc94be9403db4e93c1e198e5cd24
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-09-04 21:41:44 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,3 +1,43 @@
+class A
+
+{
+
+public:
+
+  void f(){ cout<<"A::f()"<<endl; }
+
+  int a;   
+
+};
+
+
+
+class B : public A
+
+{
+
+public:
+
+  void f(int a){cout<<"B::f()"<<endl;}
+
+  int a;
+
+};
+
+
+
+int main()
+
+{
+
+  B b;
+
+  b.f();
+
+  return 0;
+
+}
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
