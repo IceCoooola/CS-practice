1156fa1ad1f451cd247a0d1e005e0d5a64cd3f96
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-09-04 21:53:27 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,53 +1,45 @@
+class Base1 { public: int _b1; };
+class Base2 { public: int _b2; };
+class Derive : public Base1, public Base2 
+{ public: int _d; };
+
+
+int main(){
+
+Derive d;
+Base1* p1 = &d;
+Base2* p2 = &d;
+Derive* p3 = &d;
+return 0;
+}
+
+
 class A
-
 {
-
 public:
-
   A() { cout<<"A::A()"<<endl; }
-
   ~A() { cout<<"A::~A()"<<endl; }
-
   int a;
-
 };
-
-
 
 class B : public A
-
 {
-
 public:
-
   B() { cout<<"B::B()"<<endl; }
-
   ~B() {cout<<"B::~B()"<<endl; }
-
   int b;
-
 };
 
-
-
 void f()
-
 {
-
   B b;
-
 }
 
 class A
-
 {
-
 public:
-
   void f(){ cout<<"A::f()"<<endl; }
-
   int a;   
-
 };
 
 
