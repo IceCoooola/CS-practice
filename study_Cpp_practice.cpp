4531e76f508e93440c740d529db01492f0efec47
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-09-28 23:25:10 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,3 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        if(root == nullptr)
+        {
+            return 0;
+        }
+        if(root->left == nullptr && root->right == nullptr)
+        {
+            return 0;
+        }
+        if(root->left == nullptr)
+        {
+            return (root->val + findTilt(root->right));
+        }
+        else if(root->right == nullptr)
+        {
+            return(root->val + findTilt(root->left));
+        }
+        else
+        {
+            return(abs(root->left->val - root->right->val));
+        }
+        return root->val;
+    }
+};
+
 /*
 struct TreeNode {
 	int val;
