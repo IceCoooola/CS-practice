77e5dc9a556fe4eda1756100c5eea4ace277850b
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-11-18 22:46:09 -0800

    Update Ds_program4.cpp

--- Ds_program4.cpp
+++ Ds_program4.cpp
@@ -1,5 +1,5 @@
 //  main.cpp
-//  program 5
+//  program 4
 //
 //  Created by Di Bao on 11/15/22.
 //
@@ -7,27 +7,18 @@
 #include <fstream>
 #include <vector>
 #include <utility>
+#include <sstream>
 using namespace std;
 
-template<class Key, class Value>
 struct Node
 {
-    Key _key;
-    Value _value;
+    string _key;
+    int _value;
     Node* _next;
-    Node(const pair<Key, Value>& data):_key(data.first),_value(data.second),_next(nullptr){}
+    Node(const pair<string, int>& data):_key(data.first),_value(data.second),_next(nullptr){}
 };
 
-template<class K>
-struct Hash{
-    size_t operator()(const K& key)
-    {
-        return key;
-    }
-};
-
-template<>
-struct Hash<string>
+struct Hash
 {
     size_t operator()(const string& key)
     {
@@ -42,23 +33,46 @@ struct Hash<string>
     }
 };
 
-template<class Key, class Value>
 class HashTable
 {
-    typedef Node<Key, Value> HashNode;
+    typedef Node HashNode;
 public:
     HashTable(const int& size)
     {
         _hashTable.resize(size, nullptr);
     }
     
-    bool insert(const pair<Key, Value>& data)
+    bool insert(const pair<string, int>& data)
     {
         HashNode* ret = find(data.first);
         //check if data already exist
         if(ret)
             return false;
-        int i = hash<Key>(data.first);
+        ++_n;
+        if(_n == _hashTable.size())
+        {
+            vector<HashNode*> newTable;
+            int size = _n * 2;
+            newTable.resize(size, nullptr);
+            for(int i = 0; i < _hashTable.size();i++)
+            {
+                HashNode* cur = _hashTable[i];
+                while(cur)
+                {
+                    HashNode* next = cur->_next;
+                    Hash hf;
+                    int hashVal = hf(cur->_key);
+                    size_t index = hashVal % newTable.size();
+                    cur->_next = newTable[index];
+                    newTable[index] = cur;
+                    cur = next;
+                }
+                _hashTable[i] = nullptr;
+            }
+            _hashTable.swap(newTable);
+        }
+        Hash hf;
+        int i = hf(data.first) % _hashTable.size();
         HashNode* cur = _hashTable[i];
         if(!cur)
         {
@@ -75,10 +89,10 @@ public:
         
     }
     
-    HashNode* find(const Key& key)
+    HashNode* find(const string& key)
     {
-        
-            int i = hash<Key>(key);
+            Hash hf;
+            int i = hf(key) % _hashTable.size();
             HashNode* cur = _hashTable[i];
             while(cur)
             {
@@ -90,10 +104,50 @@ public:
         return nullptr;
     }
     
+    int& operator[](const string& key)
+    {
+        HashNode* ret = find(key);
+        if(!ret)
+        {
+            throw;
+        }
+        return ret->_value;
+    }
+    
+    void printTable()
+    {
+        for(int i = 0; i< _hashTable.size(); i++)
+        {
+            HashNode* j = _hashTable[i];
+            while(j)
+            {
+                cout<< j->_key<<" is "<<j->_value << endl;
+                j = j->_next;
+            }
+        }
+    }
+    
 private:
     vector<HashNode*> _hashTable;
+    size_t _n;
 };
 
 int main() {
+    HashTable ht(25);
+    ht.insert(make_pair("test1", 1));
+    ht.insert(make_pair("test", 2));
+    ht.insert(make_pair("test3", 3));
+    ht.insert(make_pair("test22", 22));
+    ht.insert(make_pair("test222", 2222));
+    ht.printTable();
+    ifstream ifs("text.txt");
+    string str;
+    while(getline(ifs, str))
+    {
+        stringstream stringstr(str);
+        vector<string> vct;
+
+    }
+
     return 0;
 }
