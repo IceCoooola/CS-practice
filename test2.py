a4691799796d4f38fa3e74adf50048805095d2bf
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-01-23 12:06:13 -0800

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,12 @@
+score1 = int(input('enter the test score 1'))
+score2 = int(input('enter the test score 2'))
+score3 = int(input('enter the test score 3'))
+avg = (score1 + score2 + score3) / 3
+if avg >= 90:
+    print('Grade A')
+elif avg >= 80:
+    print('Grade B')
+elif avg >= 70:
+    print('Grade C',"23123"*6)
+else:
+    print('Grade F')
