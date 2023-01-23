9b2aff704de3200f524f6f97774aacbb25b650fa
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-01-23 12:05:47 -0800

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,21 @@
+purchase = int(input("enter the amount of purchase = "))
+countyTax = purchase * 0.025
+stateTax = purchase * 0.05
+ttlTax = countyTax + stateTax
+ttlSale = ttlTax + purchase
+print('The purchase amount is',format(purchase, ',.2f'))
+print('The county tax is',format(countyTax, ',.2f'))
+print('The state tax is',format(stateTax, ',.2f'))
+print('The total tax is', format(ttlTax, ',.2f'))
+print('The total amount of sale is',format(ttlSale, '.2f'))
+#  / float division
+# // interger division
+# ** exponential
+# not LOGICAL NOT
+# or LOGICAL OR
+# and LOGICAL AND
+# \' zhuan yi zi fu
+# if condition:
+#   tab  +  statement...
+#   tab + statement...
+
