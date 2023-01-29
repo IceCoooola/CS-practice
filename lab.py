decb1c29425a4e2387275bdfb30fd6431149fd24
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-01-28 17:37:02 -0800

    Create lab.py

---
+++ lab.py
@@ -0,0 +1,27 @@
+42/5
+8.4
+42//5
+8
+42%5
+2
+40%5
+0
+1%2
+1
+2%1
+0
+45+4*4-2
+59
+45+43%5*(23*3%2)
+48
+5**2
+25
+5.1**2
+26.009999999999998
+radius = float(input('enter radius:> '))
+length = float(input('enter length:> '))
+pi = 3.141
+area = pi * radius ** 2
+volume = area * length
+print('The area is %.2f\n',area)
+print('volume is %.2f\n',volume)
