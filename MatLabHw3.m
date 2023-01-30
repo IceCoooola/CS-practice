1704f843ae2993332b8af2e0e678bbf63b64ebda
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-01-29 21:36:04 -0800

    Update MatLabHw3.m

--- MatLabHw3.m
+++ MatLabHw3.m
@@ -8,3 +8,20 @@ function modifiedReadings = CalibrateReadings(originalReadings, adjustVal)
 end
 CalibrateReadings([51, 53, 61, 62], 5)
   
+function countValues = CreateArray(endValue)
+% endValue: Ending value of array countValues
+countValues = (1:endValue)
+    % Construct a row array countValues from 1 to endValue, 
+    % using the colon operator
+ 
+
+end
+CreateArray(8)
+function attendanceValues = ShiftValues(attendanceValues)
+% attendanceValues: Array contains 4 elements recording attendence for the last 4 shows
+    attendanceValues(end + 1) = attendanceValues(end);
+    % Change indexing values
+   attendanceValues(1) = [];
+    
+end
+ShiftValues([25, 50, 75, 100])
