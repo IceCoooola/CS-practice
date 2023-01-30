e68a762ff176b62759e4b13274b135d018cd3a01
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-01-29 22:44:18 -0800

    Update MatLabHw3.m

--- MatLabHw3.m
+++ MatLabHw3.m
@@ -25,3 +25,24 @@ function attendanceValues = ShiftValues(attendanceValues)
     
 end
 ShiftValues([25, 50, 75, 100])
+function overweightBaggage = CheckWeight(baggageWeight, maximumWeight)
+% baggageWeight: Array of baggage weights in pounds
+% maximumWeight: Maximum baggage weight in pounds
+overweightBaggage = baggageWeight > maximumWeight;
+    % Assign overweightBaggage with true wherever baggageWeight 
+    % is above maximumWeight
+
+
+end
+CheckWeight([28, 45, 55, 30], 50)
+
+function totalYen = ConvertToYen(totalDollars)
+% totalDollars: Amount to convert in dollars
+    totalYen = floor(totalDollars * 119.771);
+    % Assign totalYen with the amount of yen distributed given totalDollars 
+    % Exchange rate is 119.771 Japanese yen for 1 US dollar
+
+
+end
+
+ConvertToYen(325)
