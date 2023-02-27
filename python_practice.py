3c62c60d4e9e0de8686bf73f24a4edb548072b70
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-02-27 11:40:04 -0800

    Update python_practice.py

--- python_practice.py
+++ python_practice.py
@@ -1,3 +1,17 @@
+
+def add(a = 10, b = 20, c = 30) -> int:
+    return a + b + c
+
+
+def main():
+    print(add(1, 2, b = 3))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
 def max(num1:int, num2 : int) -> int :
     '''compare two numbers and return larger value'''
     if num1 > num2:
