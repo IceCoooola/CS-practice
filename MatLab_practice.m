4273ee947d9fa643544c2f2437367e8b70070ede
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-04-25 17:43:17 -0700

    Update MatLab_practice.m

--- MatLab_practice.m
+++ MatLab_practice.m
@@ -1,3 +1,26 @@
+function [ outputPowerInterp outputPowerSpline ] = EstimateWindTurbinePower( windSpeed )
+% EstimateWindTurbinePower: Estimates wind turbine output power using two interpolation methods
+%   Inputs: windSpeed - wind speed for power estimate
+%
+%   Outputs: outputPowerInterp - estimated output power using
+%                                linear interpolation
+%            outputPowerSpline - estimated output power using
+%                                spline interpolation
+
+
+    windSpeedData = [ 0, 2, 4, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, ...
+		      28, 30 ]; % (m/s)
+    outputPwrData = [ 0, 0, 14, 312, 546, 840, 1180, 1535, 2037, 2100, 2100, ...
+		      2100, 2100, 2100, 2100, 0, 0, 0 ]; % (kW)
+
+    % Assign outputPowerInterp with linear interpolation estimate of output power
+    outputPowerInterp = interp1(windSpeedData, outputPwrData, windSpeed); 
+
+    % Assign outputPowerSpline with spline interpolation estimate of output power
+    outputPowerSpline = spline(windSpeedData, outputPwrData, windSpeed); 
+
+end
+
 % example 1
 clear;clc;
 a = arduino;
