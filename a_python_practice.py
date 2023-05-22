d2ce007352359534a698f600f3388641152c4054
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-05-21 22:53:39 -0700

    Update a_python_practice.py

--- a_python_practice.py
+++ a_python_practice.py
@@ -1,4 +1,27 @@
-
+import turtle
+turtle.hideturtle()
+turtle.pensize(3)
+turtle.penup()
+turtle.goto(-200, -50)
+turtle.pendown()
+turtle.circle(40, steps = 3) # Draw a triangle
+turtle.penup()
+turtle.goto(-100, -50)
+turtle.pendown()
+turtle.circle(40, steps = 4) # Draw a square
+turtle.penup()
+turtle.goto(0, -50)
+turtle.pendown()
+turtle.circle(40, steps = 5) # Draw a pentagon
+turtle.penup()
+turtle.goto(100, -50)
+turtle.pendown()
+turtle.circle(40, steps = 6) # Draw a hexagon
+turtle.penup()
+turtle.goto(200, -50)
+turtle.pendown()
+turtle.circle(40) # Draw a circle
+turtle.do
 t = dict()
 # t.add("123","123",1)
 t = {"1","2",1}
