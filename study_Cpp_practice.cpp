c0fb44227da60d5d86535e1b320263a069dc717d
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-07-04 00:45:54 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,3 +1,25 @@
+class Solution {
+public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param nums int整型vector 
+     * @return int整型
+     */
+    int minNumberInRotateArray(vector<int>& nums) {
+        int small = nums[0];
+        for(int i = 1; i< nums.size(); i++)
+        {
+            if (nums[i] < small)
+            {
+                small = nums[i];
+            }
+        }
+        return small;
+    }
+};
+
 #include <iostream>
 #include<vector>
 using namespace std;
