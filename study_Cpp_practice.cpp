73c50b20076720c0860c887f4562d0031784e0f5
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-07-17 13:56:57 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include<string>
+#include<set>
+using namespace std;
+
+int main() {
+    string str;
+    set<char> st;
+    cin >> str;
+    for(int i = 0; i < str.length(); i++)
+    {
+        st.insert(str[i]);
+    }
+    cout << st.size();
+}
+// 64 位输出请用 printf("%lld")
+
+#include <iostream>
 using namespace std;
 #include<string>
 int main() {
