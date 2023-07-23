4aa433a5e9f9a4a429bdb109f82a18bd13bea01f
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-07-23 00:46:12 -0700

    Update study_Cpp_practice.cpp

--- study_Cpp_practice.cpp
+++ study_Cpp_practice.cpp
@@ -1,4 +1,30 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+    int n;
+    int count = 0;
+    cin >> n;
+    for(int i = 2; i < n; i++)
+    {
+        int sum = 1;
+        for(int j = 2; j < sqrt(i); j++)
+        {
+            if(i % j == 0)
+            {
+                sum += j;
+                if(j != sqrt(i))
+                    sum += i/j;
+            }       
+        }
+        if(sum == i)
+            count++;
+    }
+    cout<<count;
+}
+
+#include <iostream>
 using namespace std;
 
 int main() {
