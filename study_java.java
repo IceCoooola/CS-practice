56f4492c2983f130e9751c748abbcdaa75271a7f
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-07-24 13:35:40 -0700

    Update study_java.java

--- study_java.java
+++ study_java.java
@@ -1,3 +1,31 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+        //标签
+	outer:
+        for (int i = 1; i <= 100; i++) {
+
+            // Press Ctrl+D to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Cmd+F8.
+            System.out.println("i = " + i);
+            while(i <= 5)
+            {
+                i += 1;
+                System.out.println("inner: i = "+i);
+                if(i == 5)
+                    break outer;//直接break标签层的循环
+            }
+            System.out.println("i = "+i);
+        }
+    }
+}
+
 import java.util.*; // *代表所有
 import java.util.Scanner; //在java.util 里面将scanner拿过来用
 public class test3{
