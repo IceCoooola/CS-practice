2ea6ecec66669dee16c7e686c34517b0dfeaf295
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-03 17:57:32 -0700

    Update study_java.java

--- study_java.java
+++ study_java.java
@@ -1,3 +1,27 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = {8,9,1,24,28,32,9,45,60,95,64,74,99};
+        for(int i = 1; i < arr.length; i++)
+        {
+            int key = arr[i];
+            for(int j = i - 1; j >= 0; j--)
+            {
+                if(key <= arr[j])
+                {
+                    arr[j+1] = arr[j];
+                }
+                else {
+                    arr[j] = key;
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
 import java.util.ArrayList;
 
 public class blockSearch {
