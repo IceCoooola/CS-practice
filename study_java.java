293003aa181de97ed6b7a065df411b6e516bf040
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-07 22:49:10 -0700

    Update study_java.java

--- study_java.java
+++ study_java.java
@@ -1,3 +1,13 @@
+    public static void main(String args[])
+    { Integer a = 127;
+        Integer b = 127;
+        Integer c = 128; Integer d = 128;
+        System.out.println(a == b);
+        System.out.println(c == d);
+
+    }
+}
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
