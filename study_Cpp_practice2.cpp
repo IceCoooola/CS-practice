242abf245e7fe71ea514cd06dd95c3015467f642
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-12 16:14:35 -0700

    Create study_Cpp_practice2.cpp

---
+++ study_Cpp_practice2.cpp
@@ -0,0 +1,18 @@
+bool solution(vector<int> nums, int k) {
+    map<int, int> mp;
+    
+    for(int i = 0; i < nums.size(); ++i)
+    {
+        if(mp.find(nums[i]) == mp.end())
+        {
+            mp[nums[i]] = i;
+        }
+        else {
+            if(i - mp[nums[i]] <= k)
+                return true;
+            else
+                mp[nums[i]] = i;
+        }
+    }
+    return false;
+}
