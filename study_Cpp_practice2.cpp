ec9a9f1423ccceb69426205e4b7115b9674ae5f8
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-12 17:46:41 -0700

    Update study_Cpp_practice2.cpp

--- study_Cpp_practice2.cpp
+++ study_Cpp_practice2.cpp
@@ -1,3 +1,11 @@
+int solution(int n) {
+    if(n == 1)
+        return 1;
+    if(n == 2)
+        return 2;
+    return solution(n-1) + solution(n-2);
+}
+
 //
 // Binary trees are already defined with this interface:
 // template<typename T>
