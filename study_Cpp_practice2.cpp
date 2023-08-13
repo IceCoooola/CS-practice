a8763a730b5213e2c8cb2320f42f78cd0239f50b
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2023-08-12 17:02:01 -0700

    Update study_Cpp_practice2.cpp

--- study_Cpp_practice2.cpp
+++ study_Cpp_practice2.cpp
@@ -1,3 +1,32 @@
+//
+// Binary trees are already defined with this interface:
+// template<typename T>
+// struct Tree {
+//   Tree(const T &v) : value(v), left(nullptr), right(nullptr) {}
+//   T value;
+//   Tree *left;
+//   Tree *right;
+// };
+
+bool isSymmetry(Tree<int> * left, Tree<int> * right)
+{
+    if(left == nullptr && right == nullptr)
+        return true;
+    if(left == nullptr ||right == nullptr)
+        return false;
+    if(right->value != left->value)
+        return false;
+    return isSymmetry(left->left, right->right) && isSymmetry(left->right, right->left);
+}
+
+bool solution(Tree<int> * t) {
+    if(!t)
+        return true;
+    return isSymmetry(t->left, t->right);
+}
+
+
+
 bool solution(vector<int> nums, int k) {
     map<int, int> mp;
     
