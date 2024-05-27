ba2d1d1e1c891231da904028775cec71eabfe614
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2024-05-27 00:55:08 -0700

    Update A_test.rs

--- A_test.rs
+++ A_test.rs
@@ -10,3 +10,14 @@
     println!("s1 = {}, s2 = {}", s1, s2);
 
 
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
